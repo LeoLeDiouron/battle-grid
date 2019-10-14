ea47c8b893f198341ffc01fd26ce98e095489661
L LD <leo.le-diouron.pro@gmail.com>
2019-10-14 19:10:07 +0200

    AI can attack - no nbAttack

--- ai/play_game.py
+++ ai/play_game.py
@@ -26,6 +26,12 @@ def find_unit_with_id(status_game, army, id_unit):
             return unit
     return None
 
+def find_avaible_unit(status_game):
+    for unit in status_game['myArmy']:
+        if unit['idx'] != 0 and unit['nbAttack'] > 0:
+            return unit['idx']
+    return 0
+
 def is_in_range(unit, enemy_x, enemy_y, coeff):
     if enemy_x >= unit['x'] - (unit['move'] * coeff - unit['range']) and enemy_x <= unit['x'] + (unit['move'] * coeff - unit['range']) and enemy_y >= unit['y'] - (unit['move'] * coeff - unit['range']) and enemy_y <= unit['y'] + (unit['move'] * coeff - unit['range']):
         return True
@@ -65,24 +71,48 @@ def can_attack(status_game, unit, id_closest_enemy):
     else:
         return None
 
-def move_to_enemy(unit, id_closest_enemy):
-    return 0, 1
+def move_to_enemy(status_game, unit, id_closest_enemy):
+    enemy_unit = find_unit_with_id(status_game, 'enemyArmy', id_closest_enemy)
+    x_direction = 1 if enemy_unit['x'] > unit['x'] else -1
+    y_direction = 1 if enemy_unit['y'] > unit['y'] else -1
+    offset_x = 0
+    offset_y = 0
+    while offset_x <= unit['move'] and offset_y <= unit['move']:
+        offset_x += x_direction
+        offset_y += y_direction
+        tmp_unit = unit
+        tmp_unit['x'] = unit['x'] + offset_x
+        tmp_unit['y'] = unit['y'] + offset_y
+        if is_in_range(tmp_unit, enemy_unit['x'], enemy_unit['y'], 1):
+            print("%s:%s" % (offset_x, offset_y))
+            return offset_x, offset_y
+    return 0, 0
+
+
+def attack(id_room, id_player, unit_idx, enemy_idx):
+    print("Attack")
+    body = {
+        "idx": unit_idx,
+        "idxEnemy": enemy_idx
+    }
+    post(BASE_URL + "/attack/" + id_room + "?idPlayer=" + id_player, body)
 
 def do_action(id_player, id_room, status_game):
-    offsetX = 0
-    offsetY = 1
-    unit = status_game['myArmy'][1]
+    offset_x = 0
+    offset_y = 2
+    idx_unit = find_avaible_unit(status_game)
+    unit = status_game['myArmy'][idx_unit]
     id_closest_enemy = check_enemies_in_vision(status_game, unit['x'], unit['y'])
     if id_closest_enemy is not None:
         if can_attack(status_game, unit, id_closest_enemy) == True:
-            # attack
-            pass
+            attack(id_room, id_player, unit['idx'], id_closest_enemy)
+            return
         else:
-            offset_x, offset_y = move_to_enemy(unit, id_closest_enemy)
+            offset_x, offset_y = move_to_enemy(status_game, unit, id_closest_enemy)
     body = {
-        "idx": 1,
-        "x": unit['x'] + offsetX,
-        "y": unit['y'] + offsetY
+        "idx": idx_unit,
+        "x": unit['x'] + offset_x,
+        "y": unit['y'] + offset_y
     }
     post(BASE_URL + "/move/" + id_room + "?idPlayer=" + id_player, body)
 
