6fe9c19c00fdb9b09bfeb81e8617b0c633a95178
L LD <leo.le-diouron.pro@gmail.com>
2019-10-19 15:03:16 +0200

    Clean index.html

---
+++ views/js/initItems.js
@@ -0,0 +1,72 @@
+function initMenuItems() {
+    return {
+        title: null,
+        graphics: null,
+        buttons: {},
+        text: {},
+        images: []
+    };
+}
+
+function initWaitingItems() {
+    return {
+        title: null,
+        graphics: null,
+        room: null
+    };
+}
+
+function initJoinItems() {
+    return {
+        title: null,
+        graphics: null,
+        input: null,
+        buttonValidation: null,
+        backButton: null,
+        errorMessage: null,
+        room: null
+    };
+}
+
+function initSelectionLeaderItems() {
+    return {
+        title: null,
+        shadowTitle: null,
+        buttons: {},
+        text: {},
+        border: null
+    };
+}
+
+function initSelectionItems() {
+    return {
+        title: null,
+        shadowTitle: null,
+        buttons: {},
+        text: {},
+        images: {}
+    };
+}
+
+function initGameItems() {
+    return {
+        graphics: null,
+        buttons: {},
+        text: {},
+        images: {},
+        yourTurnAnimation: {
+            sprite: null,
+            anim: null
+        }
+    };
+}
+
+function initWinnerItems() {
+    return {
+        title: null,
+        icon: null,
+        buttonRetry: null,
+        buttonBack: null
+    }
+}
+
